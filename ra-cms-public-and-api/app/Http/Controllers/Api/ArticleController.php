28634ed7e1b5adcd9c14a83942266fcb0c260189
Raiper34 <raipergm34@gmail.com>
2018-07-08 15:40:37 +0200

    article routes, controler, resource and db manipulation

---
+++ ra-cms-public-and-api/app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Resources\Article as ArticleResource;
+use App\Article;
+
+class ArticleController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ArticleResource::collection(Article::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = new Article;
+
+        $article->title = $request->title;
+        $article->content = $request->article_content;
+        $article->description = $request->description;
+        $article->keywords = $request->keywords;
+        $article->url = $request->url;
+
+        Auth::user()->articles()->save($article);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new ArticleResource(Article::find($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->article_content;
+        $article->description = $request->description;
+        $article->keywords = $request->keywords;
+        $article->url = $request->url;
+
+        $article->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::find($id);
+        $article->delete();
+    }
+}
