0e25d3ccacce1e3454cbb9716f811f2e3d9fbf96
Raiper34 <raipergm34@gmail.com>
2018-07-17 18:37:20 +0200

    article and user model

---
+++ ra-cms-admin/src/app/shared/models/article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+  id: number;
+  title: string;
+  content: string;
+  description: string;
+  keywords: string;
+  url: string;
+  created_at: number;
+  updated_at: number;
+}
---
+++ ra-cms-admin/src/app/shared/models/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  created_at: number;
+  updated_at: number;
+}
